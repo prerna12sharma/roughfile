06ccb265f5ae5e90780dcc806d055e3000c56107
prerna12sharma <98999154+prerna12sharma@users.noreply.github.com>
2022-11-29 11:46:06 +0530

    Add files via upload

---
+++ qus.js
@@ -0,0 +1,14 @@
+// console.log("hello")
+// console.log("hello world")
+
+// import React from "react";
+// import ReactDom from "react-dom/client";
+// ReactDom.render(<h1> PRERNA SHARMA</h1>,
+// document.getElementById("root"));
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
+
+              
